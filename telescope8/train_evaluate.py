6e16faf8c6b8fb862547895ab4c7423c38997fa3
max-zhao0 <105580640+max-zhao0@users.noreply.github.com>
2022-08-25 11:48:17 -0700

    Update train_evaluate.py

--- telescope8/train_evaluate.py
+++ telescope8/train_evaluate.py
@@ -6,11 +6,26 @@ import h5py
 import utils
 
 def generate_random_tracks(data, key, fake_per_real=9):
+    """
+    Generates tracks to for training data. All real tracks are included along with a number of random tracks determined by fake_per_real.
+    
+    Input
+    ---
+    data        : hit data                      : float     (# of events, # of layers, max # of particles, # of hit parameters)
+    key         : particle ids for each hit     : float64   (# of events, # of layers, max # of particles)
+    
+    Output
+    ---
+    features    : List of tensors for collections of tracks of different length from 1 to # of layers
+    labels      : List of tensors for binary label indicating if each track in feature is a real or randomly generated one
+    distr       : List of int for number of each tracks per each length
+    """
     assert data.shape[:3] == key.shape
     tracks = [[] for _ in range(data.shape[1])]
     labels = [[] for _ in range(data.shape[1])]
     for event_no in range(data.shape[0]):
-        #Find real tracks
+        
+        # Find real tracks
         nrealtracks = 0
         for hit_no in range(data.shape[2]):
             if key[event_no,0,hit_no] != 0:
@@ -28,7 +43,9 @@ def generate_random_tracks(data, key, fake_per_real=9):
                         break
                 tracks[len(real_track)-1].append(tf.stack(real_track))
                 labels[len(real_track)-1].append(1)
-        #Generate fake tracks
+                
+        # Generate fake tracks
+        # May accidentally generate real tracks, but should have negligible effect on training as events become denser
         for _ in range(nrealtracks * fake_per_real):
             hit_nos = np.random.randint(0, data.shape[2], data.shape[1])
             first_hit_no = np.random.randint(0, nrealtracks)
@@ -40,6 +57,7 @@ def generate_random_tracks(data, key, fake_per_real=9):
                     break
             tracks[len(fake_track)-1].append(tf.stack(fake_track))
             labels[len(fake_track)-1].append(0)
+      
     return [tf.stack(collec) for collec in tracks], [tf.constant(collec) for collec in labels], [len(collec) for collec in tracks]
 
 def main(argv):
@@ -54,6 +72,7 @@ def main(argv):
     print(train_feat[-1].shape)
     test_feat, test_labels, test_dist = generate_random_tracks(test_data, test_key)
 
+    # Stacked LSTM network structure arbitrarily chosen currently
     Evaluation = keras.Sequential([
         keras.layers.LSTM(32, return_sequences=True),
         keras.layers.LSTM(16, return_sequences=True),
@@ -68,6 +87,7 @@ def main(argv):
     )
     
     def training_generator(features, labels, dist, batch_size):
+        # Generates training batches for LSTM input
         assert len(features) == len(labels) == len(dist)
         prob_nhits = np.array(dist) / np.sum(dist)
         shuffled_index = [tf.random.shuffle(tf.range(length)) for length in dist]
@@ -75,20 +95,21 @@ def main(argv):
         shuffled_lab = [tf.gather(labels[i], shuffled_index[i]) for i in range(len(labels))]
         curr_track = np.zeros(len(features))
         while True:
+            # Ensures that each track in dataset is equally likely to be selexted
             nhit_index = np.random.choice(np.arange(len(features)), p=prob_nhits)
             batch_index = tf.cast(tf.range(curr_track[nhit_index], curr_track[nhit_index] + batch_size) % dist[nhit_index], tf.int32)
-            #print(batch_index)
-            #print(nhit_index)
             batch_feat = tf.gather(features[nhit_index], batch_index)
             batch_lab = tf.gather(labels[nhit_index], batch_index)
             yield batch_feat, batch_lab
+            # Stores current place in the dataset so that all tracks of a particlar size are fed before returning to the beginning
             curr_track[nhit_index] += batch_size
 
     Evaluation.fit(
         training_generator(train_feat, train_labels, train_dist, 32),
-        steps_per_epoch=2150,
+        steps_per_epoch=2150, # Number of tracks is approximately 2150 * 32
         epochs=2
     )
+    # To do: build testing loop to evaluate model
     Evaluation.save(model_path)
 
     print("Finished")
